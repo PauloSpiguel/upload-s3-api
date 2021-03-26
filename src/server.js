0539f80e060a5d413635a1b393b73a2c2d78fe0b
Paulo Spiguel <paulospiguel@hotmail.com>
2021-03-26 17:11:28 +0000

    add port config

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const morgan = require("morgan");
 const path = require("path");
 const cors = require("cors");
 
+const PORT = process.env.APP_PORT || 5000;
+
 const profilesRouter = require("./routes/profile.routes");
 const productsRouter = require("./routes/products.routes");
 const filesRouter = require("./routes/files.routes");
@@ -30,6 +32,6 @@ app.use("/profile", profilesRouter);
 app.use("/product", productsRouter);
 app.use("/products", productsRouter);
 
-app.listen(5000, () => {
-  console.log("🚀 Server is a running!");
+app.listen(PORT, () => {
+  console.log(`🚀 Server is a running on port ${PORT}!`);
 });
